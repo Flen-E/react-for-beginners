a3ed38af64c40b579d684b7e66552ab639c00d3d
Flen-E <cjj642@naver.com>
2023-08-16 02:44:16 +0900

    TutorialDone

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+import {useState, useEffect} from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword,setKeyword] = useState("")
+  const onClick = () => {
+    setCounter((prev) => prev +1);
+  }
+  const onChange = (e) => {
+    setKeyword(e.target.value);
+  }
+  // console.log("i run all the time")
+  
+  useEffect(()=>{
+    console.log("i run only once.")
+  }, []);
+  useEffect(()=>{
+    if(keyword !== "" && keyword.length > 5)
+      console.log("I run when 'keyword' changes.")
+  }, [keyword]);
+  useEffect(()=>{
+    console.log("I run when 'counter' changes.")
+  }, [counter]);
+  useEffect(()=>{
+    console.log("I run when 'keyword & counter' changes.")
+  }, [keyword, counter]);
+  
   return (
     <div >
-      <h1>Welcome back!</h1>
+      <input value = {keyword} onChange = {onChange} type="text" placeholder="Search here..."/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
